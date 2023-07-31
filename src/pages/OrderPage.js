826337195d9be9f170aabf0bd670234c521d348c
Akash Mandal <akashmandal6297@gmail.com>
2023-07-31 10:03:20 +0530

    Update

--- src/pages/OrderPage.js
+++ src/pages/OrderPage.js
@@ -11,6 +11,7 @@ const OrderPage = ({ signedInUser }) => {
       if (signedInUser) {
         // Make an API call to fetch orders for the signed-in user's email ID
         axios.get(`https://trible-trands.onrender.com/api/orders/${signedInUser.email}`)
+        // axios.get(`http://localhost:5000/api/orders/${signedInUser.email}`)
           .then((response) => {
             setOrders(response.data);
           })
@@ -19,6 +20,17 @@ const OrderPage = ({ signedInUser }) => {
           });
       }
     }, [signedInUser]);
+
+    const getStatusIcon = (status, currentStatus) => {
+      if (status === currentStatus) {
+        // return <span>&#10003;</span>; // Tick mark icon
+        return <span style={{ color: 'green' }}>&#10003;</span>;
+
+      } else {
+        return null;
+      }
+    };
+
   return (
     <div className="order-list"> {/* Add the "order-list" class */}
       <h2>Order Details</h2>
@@ -28,7 +40,6 @@ const OrderPage = ({ signedInUser }) => {
         <ul>
           {orders.map((order) => (
             <li key={order._id}>
-             
               <p><strong className='order'>Name:</strong> <strong>{order.name}</strong></p>
               <p><strong className='order'>Email: </strong> <strong>{order.email}</strong></p>
               <p><strong className='order'>Phone:</strong> <strong>{order.phone}</strong> </p>
@@ -44,7 +55,13 @@ const OrderPage = ({ signedInUser }) => {
               
               <p><strong className='order'>Total Price:</strong> <strong> ${order.totalPrice.toFixed(2)}</strong></p>
               <p><strong className='order'>Payment Method:</strong> <strong> {order.isCashOnDelivery ? 'Cash on Delivery' : 'Online Payment'}</strong></p>
-
+              <div className="order-status-bar">
+              <h3>Order Status:</h3>
+                <span>Order Confirmed--{getStatusIcon('Order-Confirmed', order.status)}--</span>
+                <span>Processing--{getStatusIcon('Processing', order.status)}--</span>
+                <span>Shipped--{getStatusIcon('Shipped', order.status)}--</span>
+                <span>Delivered--{getStatusIcon('Delivered', order.status)}--</span>
+              </div>
               
               
             </li>
